a665beb6e7519c7870598e0519df532f41fc89d5
Anne-Lie Bäck <a-l.back@outlook.com>
2021-01-07 21:04:28 +0100

    extracts select input to their own components

---
+++ client/src/components/RecipeTemplate/DifficultyInput.js
@@ -0,0 +1,19 @@
+import { styled } from 'styletron-react';
+
+const Wrapper = styled('div', {
+    margin: '0.5rem 0'
+});
+
+const DifficultyInput = ({handleChange}) => (
+    <Wrapper>
+        <label for="difficulty">Svårighetsgrad:</label>
+        <select name="difficulty" id="difficulty" onChange = {handleChange}>
+            <option value="lätt">LÄTT</option>
+            <option value="medel">MEDEL</option>
+            <option value="omständigt">OMSTÄNDIGT</option>
+            <option value="svårt">SVÅRT</option>
+        </select>
+    </Wrapper>
+);
+
+export default DifficultyInput;
--- client/src/components/RecipeTemplate/index.js
+++ client/src/components/RecipeTemplate/index.js
@@ -2,6 +2,8 @@ import {useEffect, useHistory, useState} from 'react';
 import { styled } from 'styletron-react';
 import InputField from '../inputField';
 import CategoriesInput from './CategoriesInput';
+import CookingTimeInput from './CookingTimeInput';
+import DifficultyInput from './DifficultyInput';
 
 import Icons from '../../config/icons'
 
@@ -109,6 +111,8 @@ const RecipeTemplate = () => {
         })
     };
 
+    console.log('inputValues', inputValues);
+
     return(
         <Wrapper>
             <FormWrapper>
@@ -136,24 +140,9 @@ const RecipeTemplate = () => {
                     handleChange = {(event) => setFile(event.target.files[0])}
                 /> 
 
-                <label for="cookingTime">Beräknad tillagningstid:</label>
-                <select name="cookingTime" id="cookingTime">
-                    <option value="0-15min">0-15min</option>
-                    <option value="16-30min">16-30min</option>
-                    <option value="31-45min">31-45min</option>
-                    <option value="46-60min">46-60min</option>
-                    <option value="1h - 1:30h">1h - 1:30h</option>
-                    <option value="1:31h - 2:00h">1:31h - 2:00h</option>
-                    <option value="mer än 2:00h">mer än 2:00h</option>
-                </select>
+                <CookingTimeInput handleChange = {handleChange}/>
 
-                <label for="difficulty">Svårighetsgrad:</label>
-                <select name="difficulty" id="difficulty">
-                    <option value="lätt">LÄTT</option>
-                    <option value="medel">MEDEL</option>
-                    <option value="omständigt">OMSTÄNDIGT</option>
-                    <option value="svårt">SVÅRT</option>
-                </select>
+                <DifficultyInput handleChange = {handleChange}/>
 
                 <CategoriesInput inputValues = {inputValues} updateInputValues = {setInputValues}/>
 
