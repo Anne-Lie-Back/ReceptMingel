7b3270cdc5b5a8d9b8209b648abc461a0dd6d358
Anne-Lie Bäck <a-l.back@outlook.com>
2021-01-11 14:52:28 +0100

    adds some theme

---
+++ client/src/config/theme.js
@@ -0,0 +1,27 @@
+const THEME = {
+    colors: {
+        white: ['#ffffff'],
+        black: ['#000000'],
+        grey: ['E5E5E5'],
+        primary: ['#416788', '#DEEFFF'],
+        secondary: ['#AEE2A2'], 
+        contrast: ['#FA824C'],
+        error: 'red'
+    },
+
+    fonts: {
+        special: '"Fredoka One", cursive;',
+        text: '"Quicksand", sans-serif',
+    },
+
+    fontSizes: {
+        small: '16px',
+        normal: '18px',
+        smallHeader: '24px',
+        largeHeader: '30px',
+        hero: '80px'
+    }
+
+}
+
+export default THEME
