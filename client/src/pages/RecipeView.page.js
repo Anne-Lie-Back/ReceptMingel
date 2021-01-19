7a646bea806e4213e04753b885a27477ece2fdad
Anne-Lie Bäck <a-l.back@outlook.com>
2021-01-19 16:34:54 +0100

    removed small misshap with old recipeview showing

--- client/src/pages/RecipeView.page.js
+++ client/src/pages/RecipeView.page.js
@@ -34,7 +34,6 @@ const RecipeViewPage = () => {
                     : 
                     <RecipeView setIsEdit = {setIsEdit}
                 />}
-                <RecipeView/>
             </GridContentWrapper>
         </>
 
