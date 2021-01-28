f63397b2267e150c35f564310a34fff19130e82e
Anne-Lie Bäck <a-l.back@outlook.com>
2021-01-28 02:18:21 +0100

    login done

--- client/src/components/startPage/LogInUser/index.js
+++ client/src/components/startPage/LogInUser/index.js
@@ -27,7 +27,6 @@ const Wrapper = styled('div', {
         gridTemplateColumns: '1fr 1fr',
         gridTemplateRows: '1fr 1fr',
         columnGap: '15px',
-
     }
 });
 
@@ -44,6 +43,11 @@ const Button = styled('button', {
     color: THEME.colors.white[0],
     textTransform: 'uppercase',
 
+    ':disabled' : {
+        backgroundColor: THEME.colors.contrast[1],
+        color: THEME.colors.white[0],
+    },
+
     ':hover': {
         cursor:'pointer',
         backgroundColor:THEME.colors.black[0] 
@@ -56,6 +60,23 @@ const Text = styled('p', {
         color: THEME.colors.contrast[0]
     }
 });
+
+const ErrorMessage = styled('p', {
+    gridRow: '4/5',
+    width: '180px',
+    fontFamily: THEME.fonts.text,
+    fontSize: THEME.fontSizes.Xsmall,
+    color: THEME.colors.error,
+    letterSpacing: '0.05rem',
+    fontWeight: 700,
+    textAlign: 'center',
+
+    [media.above.mobile] : {
+        gridColumn: '1/3',
+        width: '100%',
+        fontSize: THEME.fontSizes.small,
+    }
+})
 
 const LogInUser = ({handleClick}) => {
     //TODO Add functionality
@@ -81,6 +102,16 @@ const LogInUser = ({handleClick}) => {
             [name]: value,
           });
     };
+
+    const ValidateInput = () => {
+        const validation = inputValues.password.length >= 4 && inputValues.username.length >= 4
+        
+        if(!validation){
+            return true
+        }else{
+            return false
+        }
+    }
 
     const handleLoginReq = async() => {
         await login(inputValues.username, inputValues.password);
@@ -115,8 +146,15 @@ const LogInUser = ({handleClick}) => {
                 styling = "basic"
                 handleChange = {handleChange}
             />
-            <Button onClick = {handleLoginReq}> Logga in </Button>
+            <Button 
+                onClick = {handleLoginReq} 
+                disabled = {ValidateInput()}
+            > 
+            Logga in 
+            </Button>
             <Text onClick = {handleClick}> Inte medlem? <br/> Registrera dig gratis här </Text>
+            {!loginError.isOk && <ErrorMessage>{loginError.message}</ErrorMessage>}
+            
         </Wrapper>
     );
 };
